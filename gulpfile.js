c4cbcd73a70fc2af8e1b89b76458f84f757d9a29
karthikalaxmi <karthikapurnima9@gmail.com>
2016-07-20 19:47:16 +0530

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,19 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-
-var rename = require ('gulp-rename');
-var cssmin = require ('gulp-cssmin');
-
-
-gulp.task('scripts', function() {
-    return gulp.src('src/js/*.js')
-         .pipe(uglify())
-        .pipe(gulp.dest('build/js'));
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
+gulp.task('script',function(){
+ gulp.src('src/js/*.js')
+	.pipe(rename({suffix:'.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('build/js'));
 });
-gulp.task('default', ['scripts']);
 
 
-
-
-
-gulp.task('suri',function() {
-	return gulp.src('src/scss/*.css')
+gulp.task('karthi',function(){
+	gulp.src('src/scss/*.css')
+	.pipe(rename({suffix:'.min'}))
 	.pipe(cssmin())
-	.pipe(gulp.dest ('build/css'));
+	.pipe(gulp.dest('build/css'));
 });
-gulp.task('default',['suri']);
-
-
-
-
+gulp.task('default',['script','karthi']);
