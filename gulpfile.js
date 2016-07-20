d6012b394cd3c19c6a27e1baa42453e25895c7c2
satya <dsatya66@gmail.com>
2016-07-20 19:44:27 +0530

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
+// JavaScript Document
 var gulp = require('gulp');
 
-gulp.task('hello',function(){
-	console.log('hellooooo world');
-	});
-	
-	
-var uglify = require('gulp-uglify');
-var rename =require('gulp-rename');
-var cssmin =require('gulp-cssmin');
 
-gulp.task('priya',function(){
- gulp.src('src/js/custom.js')
-	.pipe (rename({suffix:'.min'}))
-	.pipe (uglify())
-	.pipe(gulp.dest('build/js'));
-	});
-	
-	
-	gulp.task('cascade', function(){
-		gulp.src('src/css/*.css')
-		.pipe (rename ({suffix:'.min'}))
-		.pipe (cssmin())
-		.pipe(gulp.dest('build/css'));
-		});
-	gulp.task( 'default',['priya', 'cascade'] );
+ gulp.task('hai', function(){
+	 console.log('hello world--!')
+	 });
+	 
+
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
+	 
+gulp.task('satya', function(){
+  gulp.src('src/js/custom.js')
+  .pipe(uglify())
+  .pipe(rename ({suffix:'.min'}))
+  .pipe(gulp.dest('build/js'));
+});
+	 
+	 
+gulp.task('style', function(){
+  gulp.src('src/css/*.css')
+  .pipe(cssmin())
+  .pipe(rename ({suffix:'.min'}))
+  .pipe(gulp.dest('build/css'));
+});
+	 
+ gulp.task('default',['hai','satya','style']);
