aef63d4f2d1426d21c31bc690fa6fa526e483dd1
priya <priyanka.pithani@gmail.com>
2016-07-23 14:42:21 +0530

    cascade css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,9 @@
 var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-<<<<<<< e4e036721998e379158b184ea30203fe641de560
-  sass = require('gulp-sass');
+  concat = require('gulp-concat');
 
   var libjs = [
-=======
-  uglify = require('gulp-uglify');
-  
-   var libjs = [
->>>>>>> add uglify task to gulp
-    './node_modules/jquery/dist/jquery.js',
-    './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
-	
+    'node_modules/jquery/dist/core.js',
+    './node_modules/bootstrap/dist/css/bootstrap',
   ];
   
   gulp.task('libjs',function(){
@@ -20,19 +12,3 @@ var gulp = require('gulp'),
 	  .pipe(gulp.dest('./build/js'));
   });
   
-<<<<<<< e4e036721998e379158b184ea30203fe641de560
-  gulp.task('sass', function () {
-    return gulp.src('./src/sass/*.scss')
-      .pipe(sass({errLogToConsole: true, outputStyle: 'expanded'}).on('error', sass.logError))
-	  .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./build/css'));
-=======
-
-  
-  
-  gulp.task('jsfile',function(){
-	  return gulp.src('src/js/swiper.js')
-	  .pipe(uglify())
-	  .pipe(gulp.dest('build/js'));
->>>>>>> add uglify task to gulp
-  });
