d86160897ce65f349a62f634e4f6f1d31f24dd6a
RajeshKumpati <sushma.dangeti66@gmail.com>
2016-07-23 15:24:32 +0530

    add css-scss task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,28 @@ var gulp = require('gulp'),
   cssmin = require ('gulp-cssmin');
   
 var libjs = [
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  cssScss = require('gulp-css-scss');
+  var libjs = [
     './node_modules/jquery/dist/jquery.js',
     './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
-	
   ];
   
 gulp.task('libjs',function(){
-
   gulp.src(libjs)
  .pipe(concat('lib.js'))
  .pipe(gulp.dest('./build/js'));
 
 });
 
-  
-  gulp.task('sass', function () {
+gulp.task('sass', function () {
     return gulp.src('./src/sass/*.scss')
       .pipe(sass({errLogToConsole: true, outputStyle: 'expanded'}).on('error', sass.logError))
 	  .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./build/css'));
-  });
+});
   
- 
- 
 gulp.task('suri',function() {
 	return gulp.src('src/scss/*.css')
 		.pipe(cssmin())
@@ -48,3 +47,19 @@ gulp.task('jsfile',function(){
     .pipe(uglify())
     .pipe(gulp.dest('build/js'));
 });
+
+   gulp.task('jsfile',function(){
+	  return gulp.src('src/js/swiper.js')
+	  .pipe(uglify())
+	  .pipe(gulp.dest('build/js'));
+   });
+
+  
+ gulp.task('css-scss', function() {
+  return gulp.src('./src/css/style.css')
+    .pipe(cssScss())
+    .pipe(gulp.dest('./build/scss'));
+});
+
+
+>>>>>>> add css-scss task to gulp
