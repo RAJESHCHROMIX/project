0176a10ff6a944455c956fb26153939f36dd7246
sagarrayudu <sagar474@hotmail.com>
2016-07-23 10:30:27 +0530

    add gulp libjs task

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,14 @@
-// JavaScript Document
-var gulp = require('gulp')
-gulp.task('hello',function() {
-	console.log('hello zell');
-	
-});
+var gulp = require('gulp'),
+  concat = require('gulp-concat');
 
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var cssmin = require('gulp-cssmin');
-var sass = require('gulp-sass');
-
-gulp.task('jsmin',function(){
-return gulp.src('src/js/global.js')
-.pipe(rename({suffix:'.min'}))
-.pipe(uglify())
-.pipe(gulp.dest('destination/js'))
-})
-
-gulp.task('default',['jsmin']);
-
-gulp.task('cssmin',function(){
-	return gulp.src('src/css/style.css')
-	.pipe(rename({suffix:'.min'}))
-	.pipe(cssmin())
-	.pipe(gulp.dest('destination/css'))
-})
-
-gulp.task('default',['cssmin']);
-
-gulp.task('sassmin', function(){
-  return gulp.src('src/sass/forms.sass')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('destination/sass'))
-});
-
-gulp.task('default',['sassmin']);
+  var libjs = [
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
+  ];
+  
+  gulp.task('libjs',function(){
+	  gulp.src(libjs)
+	  .pipe(concat('lib.js'))
+	  .pipe(gulp.dest('./build/js'));
+  });
+  
