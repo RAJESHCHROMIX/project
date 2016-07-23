e6b984005d6b9b1decf7b8319f7b32e667636933
sagar474@hotmail.com <sagarrayudu>
2016-07-23 14:42:56 +0530

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp'),
-  concat = require('gulp-concat');
-
-  var libjs = [
-    'node_modules/jquery/dist/core.js',
-    './node_modules/bootstrap/dist/css/bootstrap',
+  concat = require('gulp-concat'),
+  sass = require('gulp-sass'),
+  uglify = require('gulp-uglify');
+  
+var libjs = [
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
+	
   ];
   
-  gulp.task('libjs',function(){
-	  gulp.src(libjs)
-	  .pipe(concat('lib.js'))
-	  .pipe(gulp.dest('./build/js'));
-  });
+gulp.task('libjs',function(){
+
+  gulp.src(libjs)
+ .pipe(concat('lib.js'))
+ .pipe(gulp.dest('./build/js'));
+
+});
+
   
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.scss')
+    .pipe(sass({errLogToConsole: true, outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'));
+
+gulp.task('jsfile',function(){
+  return gulp.src('src/js/swiper.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
