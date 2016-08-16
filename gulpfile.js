d1742eefaa5e9a5c57cf4f32c9f1e3fc294c41b4
karthikalaxmi <karthikapurnima9@gmail.com>
2016-08-16 10:18:57 +0530

    add takeaway-part

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     watch = require('gulp-watch'),
     livereload = require('gulp-livereload');
+const image = require('gulp-imagemin');	
 
 	gulp.task('build',['sass','html-copy','images'],
 	  function(){
@@ -24,13 +25,14 @@ var gulp = require('gulp'),
   });
 
  gulp.task('images', function () {
-          return gulp.src('./src/images/*.png')
+          return gulp.src('./src/images/*')
+		    .pipe(imagemin({progressive:true}))
             .pipe(gulp.dest('./build/images'));
   });
 
   gulp.task('watch', function () {
          gulp.watch('./src/**/*.scss',['sass']);
          gulp.watch('./src/**/*.html',['html-copy']);
-         gulp.watch('./src/**/*.png',['images']);
+         gulp.watch('./src/**/*.',['images']);
 
   });
