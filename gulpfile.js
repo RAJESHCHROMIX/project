f2ded85e70cee28d9b895f53f12711b319583abc
sumana0512 <sumanaalaparthi@gmail.com>
2016-08-26 12:06:11 +0530

    ourservice add

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     watch = require('gulp-watch'),
+<<<<<<< .merge_file_a09032
     livereload = require('gulp-livereload'),
     imagemin = require('gulp-imagemin');
 
 
+=======
+    images=require('gulp-images'),
+    livereload = require('gulp-livereload');
+>>>>>>> .merge_file_a09052
 	gulp.task('build',['sass','html-copy','image-copy'],
 	  function(){
 
@@ -30,9 +35,18 @@ var gulp = require('gulp'),
             .pipe(gulp.dest('./build/images'));
   });
 
+ gulp.task('images', function () {
+          return gulp.src('./src/images/*.*')
+            .pipe(gulp.dest('./build'));
+  });
+
   gulp.task('watch', function () {
          gulp.watch('./src/**/*.scss',['sass']);
          gulp.watch('./src/**/*.html',['html-copy']);
+<<<<<<< .merge_file_a09032
          gulp.watch('./src/**/*.png',['image-copy']);
 
+=======
+         gulp.watch('./src/**/*.*',['images']);
+>>>>>>> .merge_file_a09052
   });
